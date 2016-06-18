862b96b128ff52c3b2f9c00f8b7fd709ef364004
varunreddy <varun8686@gmail.com>
2016-06-18 10:15:44 -0600

    first commit

---
+++ SimpleTODO/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SimpleTODO
+//
+//  Created by Varun Reddy Nalagatla on 6/18/16.
+//  Copyright © 2016 Varun Reddy Nalagatla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
